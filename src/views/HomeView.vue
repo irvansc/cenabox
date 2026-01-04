5a978f48632393986f2eaf9dc9984eda0e1da23a
irvanscx <irvanmaulana292@gmail.com>
2026-01-04 22:12:18 +0700

    Fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
 import { ref, onMounted, onUnmounted } from 'vue';
 import HomeMobile from './HomeMobile.vue';
 import HomeDesktop from './HomeDesktop.vue';
-import { useHead } from '@unhead/vue';
+import { useHead } from '@unhead/vue/client';
 const isMobile = ref(true);
 
 const checkScreenSize = () => {
